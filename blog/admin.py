1fc06374d236823a50aab28f3b4496300b1e3649
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-09-14 00:44:46 +0530

    Registered Post model in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = "title", "slug", "author", "publish", "status",
+    list_filter = "status", "created", "publish", "author",
+    prepopulated_fields = {"slug": ("title",) }
+    raw_id_fields = "author",
+    date_hierarchy = "publish"
+    ordering = "status", "publish",
