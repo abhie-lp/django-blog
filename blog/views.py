886852908a4043e8f3a61f4ed30c7f943397a5c2
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-09-14 21:45:47 +0530

    View posts with same tag

--- blog/views.py
+++ blog/views.py
@@ -2,27 +2,32 @@ from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from taggit.models import Tag
 
-#def post_list(request):
-#    object_list = Post.published.all()
-#    paginator = Paginator(object_list, 3)
-#    page = request.GET.get("page")
-#    try:
-#        posts = paginator.page(page)
-#    except PageNotAnInteger:
-#        # If not an integer deliver the first page
-#        posts = paginator.page(1)
-#    except EmptyPage:
-#        # If page is out of range deliver last page of results
-#        posts = paginator.page(paginator.num_pages)
-#
-#    return render(request, "blog/list.html", {"page": page, "posts": posts})
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get("page")
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = "posts"
-    paginate_by = 3
-    template_name = "blog/list.html"
+    return render(request, "blog/list.html", {"page": page, "posts": posts, "tag": tag})
+
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = "posts"
+#     paginate_by = 3
+#     template_name = "blog/list.html"
 
 from .forms import CommentForm
 
