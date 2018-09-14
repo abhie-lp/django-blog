ffc943d0cfa71678b521ffc1cc45066415c1dccd
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-09-14 21:18:29 +0530

    Added comment view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
@@ -24,6 +24,7 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = "blog/list.html"
 
+from .forms import CommentForm
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
@@ -32,7 +33,23 @@ def post_detail(request, year, month, day, post):
                                     publish__month=month,
                                     publish__day=day)
 
-    return render(request, "blog/detail.html", {"post": post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            return HttpResponseRedirect("")
+    else:
+        comment_form = CommentForm()
+
+    return render(request, "blog/detail.html", {"post": post,
+                                                "comments": comments,
+                                                "new_comment": new_comment,
+                                                "comment_form": comment_form})
 
 
 from .forms import EmailPostForm
