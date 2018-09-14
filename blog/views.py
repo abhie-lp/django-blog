6a5e9c333f4ca64900651e2b1190154f06691d18
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-09-14 08:03:47 +0530

    Added views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, "blog/list.html", {"posts": posts})
+
+
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, slug=post,
+                                    status="published",
+                                    publish__year=year,
+                                    publish__month=month,
+                                    publish__day=day)
+
+    return render(request, "blog/detail.html", {"post": post})
