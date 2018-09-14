41063e8e6168fb3658dfbc32b5d60f40491db585
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-09-14 09:40:28 +0530

    View to send e-mail

--- blog/views.py
+++ blog/views.py
@@ -33,3 +33,26 @@ def post_detail(request, year, month, day, post):
                                     publish__day=day)
 
     return render(request, "blog/detail.html", {"post": post})
+
+
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status="published")
+    sent = False
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} ({cd['email']}) recommends you reading {post.title}."
+            message = f'Read "{post.title}" at {post_url}\n\n{cd["name"]}\'s comments {cd["comments"]}.'
+            send_mail(subject, message, "admin@myblog.com", [cd["to"]])
+            sent = True
+    
+    else:
+        form = EmailPostForm()
+    
+    return render(request, "blog/share.html", {"post": post, "form": form, "sent": sent})
