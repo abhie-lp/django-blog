42aa30369a3d5aee1178ba094fe788289c842c5a
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-09-15 00:01:56 +0530

    Created inclusion_tag: show_latest_posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -6,3 +6,9 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+
+@register.inclusion_tag("blog/latest_posts.html")
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by("-publish")[:count]
+    return {"latest_posts": latest_posts}
