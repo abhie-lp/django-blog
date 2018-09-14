d8aceec5011830a1ac80457a08733b4a64b0568d
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-09-14 08:53:44 +0530

    Switched to CBV for list

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)
-    page = request.GET.get("page")
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
+#def post_list(request):
+#    object_list = Post.published.all()
+#    paginator = Paginator(object_list, 3)
+#    page = request.GET.get("page")
+#    try:
+#        posts = paginator.page(page)
+#    except PageNotAnInteger:
+#        # If not an integer deliver the first page
+#        posts = paginator.page(1)
+#    except EmptyPage:
+#        # If page is out of range deliver last page of results
+#        posts = paginator.page(paginator.num_pages)
+#
+#    return render(request, "blog/list.html", {"page": page, "posts": posts})
 
-    return render(request, "blog/list.html", {"page": page, "posts": posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/list.html"
 
 
 def post_detail(request, year, month, day, post):
