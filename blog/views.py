15ae4bfb05c2afeada855607b7ae32d4363ceef4
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-09-16 12:38:31 +0530

    Adding Basic search

--- blog/views.py
+++ blog/views.py
@@ -84,3 +84,28 @@ def post_share(request, post_id):
         form = EmailPostForm()
     
     return render(request, "blog/share.html", {"post": post, "form": form, "sent": sent})
+
+
+from django.contrib.postgres.search import SearchVector
+from .forms import SearchForm
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.objects.annotate(
+                search=SearchVector("title", "body"),
+            ).filter(search=query)
+    
+    return render(
+        request,
+        "blog/post_search.html",
+        {"form": form,
+        "query": query,
+        "results":results,}
+    )
--- mysite/settings.py
+++ mysite/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS += [
     "taggit",
     "django.contrib.sites",
     "django.contrib.sitemaps",
+    "django.contrib.postgres",
 ]
 
 MIDDLEWARE = [
