7252d9a2bfcb8fc597c7b3ade3b83e60b63dc0eb
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-09-16 12:11:59 +0530

    Added postgresql database

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "blog",
+        "USER": "abhie",
+        "PASSWORD": "qwertyuiop1234",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
