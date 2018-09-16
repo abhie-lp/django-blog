3e361d8595b6daddf1d145a7d68863047a8cc5e0
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-09-16 12:47:51 +0530

    A little more advanced search

--- blog/views.py
+++ blog/views.py
@@ -98,12 +98,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            search_vector = SearchVector("title", "body")
+            search_vector = SearchVector("body", weight="A") + SearchVector("title", weight="B")
             search_query = SearchQuery(query)
             results = Post.objects.annotate(
                 search=search_vector,
                 rank=SearchRank(search_vector, search_query)
-            ).filter(search=search_query).order_by("-rank")
+            ).filter(rank__gte=0.3).order_by("-rank")
     
     return render(
         request,
