8cbfc3070f416f7e4074c7aa7ca2167d3830ca8c
Abhishek Tiwari <abhie.lp@gmail.com>
2020-09-17 11:34:59 +0530

    Form to share email

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path("", views.post_list, name="post_list"),
     path("<int:year>/<int:month>/<int:day>/<slug:post>/",
          views.post_detail, name="post_detail"),
+    path("<int:post_id>/share/", views.post_share, name="post_share"),
+    path("", views.post_list, name="post_list"),
 ]
