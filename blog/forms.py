def51c648f7be26769aa9c2bf32f85c09db9914d
Abhishek Tiwari <abhie.lp@gmail.com>
2020-09-17 14:11:12 +0530

    Comment form

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,17 @@
 from django import forms
 
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = "name", "email", "body"
+    
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields["body"].widget = forms.Textarea()
+
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=32)
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 def post_share(request, post_id):
@@ -45,4 +45,18 @@ def post_detail(request, year, month, day, post):
         publish__year=year, publish__month=month, publish__day=day
     )
     
-    return render(request, "blog/post/detail.html", {"post": post})
+    comments = post.comments.filter(active=True)
+    
+    new_comment = None
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment: Comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    
+    return render(request, "blog/post/detail.html",
+                  {"post": post, "comments": comments,
+                   "new_comment": new_comment, "comment_form": comment_form})
