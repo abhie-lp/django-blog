88d438a65b14ca86a7bad568f1589949e7c23b04
Abhishek Tiwari <abhie.lp@gmail.com>
2020-09-19 14:12:10 +0530

    Markdown filter to filter blog where markdown is used.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,9 @@
+import markdown
+
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+
 from ..models import Post
 
 register = template.Library()
@@ -20,3 +24,8 @@ def show_latest_posts(count=5):
 def get_most_commented_posts(count=5):
     return Post.published.annotate(total_comments=Count("comments"))\
                          .order_by("-total_comments")[:count]
+
+
+@register.filter(name="markdown")
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
