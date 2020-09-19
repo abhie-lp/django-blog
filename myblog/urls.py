f0856e48f7d2cf71955ec34bcfa8995b620aec22
Abhishek Tiwari <abhie.lp@gmail.com>
2020-09-19 18:10:54 +0530

    Redirect to blog list

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
+from django.views.generic.base import RedirectView
 
 from blog.sitemaps import PostSitemap
 
@@ -11,4 +12,5 @@ urlpatterns = [
     path("blog/", include("blog.urls", namespace="blog")),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps},
          name="django.contrib.sitemaps.views.sitemap"),
+    path("", RedirectView.as_view(url="/blog/"))
 ]
