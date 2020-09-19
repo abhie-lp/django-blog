f7a92f18a030bb907d137f880fc6645bc2728fe2
Abhishek Tiwari <abhie.lp@gmail.com>
2020-09-19 13:58:21 +0530

    Most commented posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 from ..models import Post
 
 register = template.Library()
@@ -13,3 +14,9 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by("-publish")[:count]
     return {"latest_posts": latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count("comments"))\
+                         .order_by("-total_comments")[:count]
